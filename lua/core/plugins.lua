45649cba72ab65529f31cc41539622183a8112a3
Fabio Teixeira <fabio1079@gmail.com>
2023-06-09 08:18:55 -0300

    Add cappyzawa/trim.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -39,6 +39,14 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
   use 'rafamadriz/friendly-snippets'
 
+
+  use({
+    "cappyzawa/trim.nvim",
+    config = function()
+      require("trim").setup({})
+    end
+  })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
